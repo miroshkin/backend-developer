433912f963c2724375d26ccd6ed493336860fa3c
Anton Miroshkin <anton.miroshkin@gmail.com>
2020-03-02 18:02:43 +0300

    Added Monitor synchronization

--- BackendDevelopment/BackendDevelopment/Program.cs
+++ BackendDevelopment/BackendDevelopment/Program.cs
@@ -15,24 +15,30 @@ namespace BackendDevelopment
         {
             for (int i = 0; i < 5; i++)
             {
-                Thread thread = new Thread(Count);
-                thread.Name = "Thread " + i.ToString();
-                thread.Start();
+                Thread myThread = new Thread(Count);
+                myThread.Name = $"Thread {i.ToString()}";
+                myThread.Start();
             }
 
             Console.ReadLine();
         }
         public static void Count()
         {
-            lock (locker)
+            bool acquiredLock = false;
+            try
             {
+                Monitor.Enter(locker, ref acquiredLock);
                 x = 1;
                 for (int i = 1; i < 9; i++)
                 {
-                    Console.WriteLine("{0}: {1}", Thread.CurrentThread.Name, x);
+                    Console.WriteLine($"{Thread.CurrentThread.Name}: {x}");
                     x++;
                     Thread.Sleep(100);
                 }
+            }
+            finally
+            {
+                if (acquiredLock) Monitor.Exit(locker);
             }
         }
     }
