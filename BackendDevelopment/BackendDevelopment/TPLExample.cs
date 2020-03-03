76aa25694170cb1d863ca78a80c933ccdad384b5
Anton Miroshkin <anton.miroshkin@gmail.com>
2020-03-03 17:40:22 +0300

    Added wait method for task array.

--- BackendDevelopment/BackendDevelopment/TPLExample.cs
+++ BackendDevelopment/BackendDevelopment/TPLExample.cs
@@ -15,6 +15,12 @@ namespace BackendDevelopment
                 return;
             }
 
+            PartOne();
+            PartTwo();
+        }
+
+        private void PartOne()
+        {
             Console.WriteLine(this.GetType().Name);
 
             Task taskOne = new Task(() => Console.WriteLine("task one - Hello!"));
@@ -23,8 +29,27 @@ namespace BackendDevelopment
             Task taskTwo = Task.Factory.StartNew(() => Console.WriteLine("task two - Hello!"));
 
             Task taskThree = Task.Run(() => Console.WriteLine("task three - Hello!"));
+
+        }
+
+        private void PartTwo()
+        {
+            Task [] taskList = new Task[3]
+            {
+                new Task(() => Console.WriteLine("first task")), 
+                new Task(() => Console.WriteLine("second task")), 
+                new Task(() => Console.WriteLine("third task"))
+            };
+
+            foreach (var task in taskList)
+            {
+                task.Start();
+            }
             
-            Console.ReadLine();
+            //Let's wait for all tasks
+            Task.WaitAll(taskList);
+
+            Console.WriteLine("The end of main method");
         }
     }
 }
